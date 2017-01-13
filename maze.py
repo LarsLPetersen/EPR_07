9f7a7bb4a5f67273a0fad48d904ad7c10168f7ca
Lars Petersen <petersen.l.lars@gmail.com>
2017-01-13 08:39:16 +0100

    Neue Dateiversionen eingepflegt.

--- maze.py
+++ maze.py
@@ -1,28 +1,26 @@
-"""Contains the Code for solving a robot's maze problem."""
-
-__author__ = "5625448: Lilian Mendoza de Sudan, 6290157: Lars Petersen"
-__copyright__ = ""
-__credits__ = "" 
-__email__ = "lilian_mendoza@hotmail.com, petersen@informatik.uni-frankfurt.de"
-
+"""
+Fle for EPR exercise 10.2.
+"""
 
 import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle, Circle
+from matplotlib.patches import Rectangle,Circle
 import numpy as np
 
 
-def display(maze, position,goal):
-    """Draws the labyringth, current position, goal and the shortest path."""
+def display(maze, position, goal):
+    """
+    draw the labyringth, current position, goal and the shortest path
+    """
     fig = plt.figure(0)
     fig.clf()
     ax = plt.gca()
     for x in range(maze.shape[1]):
         for y in range(maze.shape[0]):
-            if Maze[y, x] == 0:
-                ax.add_patch(Rectangle((x-.5, y-.5), 1., 1., alpha=1))
+            if maze[y,x] == 0:
+                ax.add_patch(Rectangle((x-.5,y-.5), 1., 1., alpha=1))
 
-    ax.add_patch(Circle((position[0], position[1]), .3, color='yellow', alpha=1))
-    ax.add_patch(Circle((goal[0], goal[1]),.3, color='red', alpha=1))
+    ax.add_patch(Circle((position[0], position[1]), .3,color='yellow', alpha=1))
+    ax.add_patch(Circle((goal[0], goal[1]), .3, color='red', alpha=1))
 
     plt.xlim(-.5, maze.shape[1]-.5)
     plt.ylim(-.5, maze.shape[0]-.5)
@@ -31,21 +29,26 @@ def display(maze, position,goal):
 
 
 def get_neighbors(maze, x, y):
-    """Uility-function to compute _valid_ neighbors of a cell."""
+    """
+    utility-function to compute _valid_ neighbors of a cell,
+    so we do not index out of bounds
+    """
     n = []
-    if x + 1 < maze.shape[1]:
-        n.append([x + 1, y])
-    if x - 1 >= 0:
+    if x+1 < maze.shape[1]:
+        n.append([x+1, y])
+    if x-1 >= 0:
         n.append([x-1, y])
-    if y + 1 < maze.shape[0]:
-        n.append([x,y + 1])
-    if y - 1 >= 0:
-        n.append([x, y - 1])
+    if y+1 < maze.shape[0]:
+        n.append([x, y+1])
+    if y-1 >= 0:
+        n.append([x, y-1])
     return n
 
 
 def create_graph(maze):
-    """Computes the graph representation of the map."""
+    """
+    compute the graph representation of the map
+    """
     graph = {}
     
     
--- sort.py
+++ sort.py
@@ -1,23 +1,27 @@
-"""Contains the Sort-Algorithms Bubblesort, Insertionsort und Quicksort."""
-
-__author__ = "5625448: Lilian Mendoza de Sudan, 6290157: Lars Petersen"
-__copyright__ = ""
-__credits__ = "" 
-__email__ = "lilian_mendoza@hotmail.com, petersen@informatik.uni-frankfurt.de"
-
+#!/usr/bin/env python
+"""
+Fle for EPR exercise 10.1.
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 from random import shuffle
 import time
 
+
 def generate_random_numbers(num):
-    """ """
+    """
+    generate num random numbers
+    """
     np.random.seed(0) # set seed so we all get the same "random" numbers
-    return np.random.choice(range(1,20), num, replace=True)
+    return np.random.choice(range(1, 20), num, replace=True)
+
 
 def display(numbers, title=""):
-    """ """
+    """
+    display results, add all the necessary things
+    to display your own results
+    """
     fig = plt.figure(0)
     plt.ion()
     plt.clf()
@@ -25,8 +29,11 @@ def display(numbers, title=""):
     plt.title(title)
     plt.pause(0.001)
 
+
 def dummy(numbers, disp=False):
-    """ """
+    """
+    only a dummy-function
+    """
     for i in range(10):
         if disp:
             display(numbers, "Dummy")
@@ -36,11 +43,11 @@ def dummy(numbers, disp=False):
 
 
 numbers = generate_random_numbers(10)
-st = time.time() # save current time
+save_time = time.time() # save current time
 sorted_numbers_dm = dummy(numbers.copy(), disp=True)
-print("Dummy took: ", time.time() - st)
+print("Dummy took: ", time.time() - save_time)
 
-if np.array_equal(sorted_numbers_dm,np.sort(numbers)):
+if np.array_equal(sorted_numbers_dm, np.sort(numbers)):
     print("OK")
 else:
     print("NOT OK")
